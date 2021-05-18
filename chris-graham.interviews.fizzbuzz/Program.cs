c487bfb0a732c3e20f7132bf5dfdc0fde6104519
Chris Graham <chrissygraham72@gmail.com>
2021-05-18 19:30:39 +0100

    Fizzbuzz solution

---
+++ chris-graham.interviews.fizzbuzz/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace chris_graham.interviews.fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MethodOne();
+            Console.ReadKey();
+        }
+
+        public static void MethodOne()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                var output = string.Empty;
+
+                if (i % 3 == 0)
+                    output += "Fizz";
+
+                if (i % 5 == 0)
+                    output += "Buzz";
+
+                Console.WriteLine(output);
+            }
+        }
+
+        public static void MethodTwo()
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+            }
+        }
+    }
+}
