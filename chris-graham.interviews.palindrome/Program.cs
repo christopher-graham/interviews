f9558a1a01ba77852ed8ea668a8f9341cd1ed511
Chris Graham <chrissygraham72@gmail.com>
2021-05-18 19:03:49 +0100

    Initial commit with palindrome program

---
+++ chris-graham.interviews.palindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+
+namespace chris_graham.interviews.palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var temp = new StringBuilder();
+
+            for(int i = input.Length -1; i >= 0; i--)
+            {
+                temp.Append(input[i]);
+
+                if(temp.ToString() == input)
+                {
+                    Console.WriteLine("String is a palindrome|");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+
+            Console.WriteLine("String is not a palindrome");
+            Console.ReadKey();
+        }
+    }
+}
